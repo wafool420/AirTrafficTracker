0101b289ca2d43f437778923904d8b3eee0b32e4
wafool420 <167025055+wafool420@users.noreply.github.com>
2025-05-14 11:44:29 +0800

    Update wsgi.py

--- AirTrafficProject/AirTrafficProject/wsgi.py
+++ AirTrafficProject/AirTrafficProject/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirTrafficProject.AirTrafficProject.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirTrafficProject.settings')
 
 application = get_wsgi_application()
