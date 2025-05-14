8c104440cb63ab32f59dcf71180187309d50c471
wafooI420 <kurtronnel761@gmail.com>
2025-05-14 15:02:49 +0800

    Added loggers

--- AirTrafficProject/AirTrafficProject/settings.py
+++ AirTrafficProject/AirTrafficProject/settings.py
@@ -16,6 +16,25 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'ERROR',
+    },
+}
+
+
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
