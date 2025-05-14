1fd8341432be838f762997f7657315164458f255
wafool420 <167025055+wafool420@users.noreply.github.com>
2025-05-14 13:17:18 +0800

    Update settings.py

--- AirTrafficProject/AirTrafficProject/settings.py
+++ AirTrafficProject/AirTrafficProject/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-c!)jua5ngw*phh!qack0%z=62z(6krvf!ur)s_l)=3()*i3o8d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['airtraffictracker.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
