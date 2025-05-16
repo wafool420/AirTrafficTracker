c9451341c125e96fe0eb90e2beaaa55ec634bfbe
wafooI420 <kurtronnel761@gmail.com>
2025-05-16 13:11:48 +0800

    Changing DB

--- AirTrafficProject/AirTrafficProject/settings.py
+++ AirTrafficProject/AirTrafficProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,10 +96,9 @@ WSGI_APPLICATION = 'AirTrafficProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('postgres://airtrafficdb_user:JLem7bUS32B2zvJ66DT4ok4pl2Yww2r1@dpg-d0jectje5dus73cbuc40-a:5432/airtrafficdb')
+    )
 }
 
 
